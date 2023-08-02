e8026d99fd06e8e895679c41de950f852f263b26
SamBorisov <88675952+SamBorisov@users.noreply.github.com>
2023-08-02 16:22:16 +0300

    .

--- src/Contracts/contracts.ts
+++ src/Contracts/contracts.ts
@@ -1,5 +1,5 @@
-export const APP_INFURA_API_KEY = '003d4e25006c4d669878e9960610f42f';
-export const EtherscanProvider_API_KEY='DJMYXYS6RAGUU4NKIU2N6WUMXH436FDY4H';
+export const APP_INFURA_API_KEY = process.env.INFURA_API_KEY;
+export const EtherscanProvider_API_KEY= process.env.ETHERSCAN_API_KEY;
 export const networks = [
   { label: 'Ethereum Kovan', value: '42' },
   { label: 'Ethereum Rinkeb', value: '4' }
